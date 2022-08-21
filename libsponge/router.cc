842b0ed45d615fb59e17cda5da9c695778ebc08c
SourVoice <2985122798@qq.com>
2022-08-21 17:14:07 +0000

    finish lab6, 100% tests passed

--- libsponge/router.cc
+++ libsponge/router.cc
@@ -16,7 +16,7 @@ using namespace std;
 // You will need to add private members to the class declaration in `router.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 //! \param[in] route_prefix The "up-to-32-bit" IPv4 address prefix to match the datagram's destination address against
 //! \param[in] prefix_length For this route to be applicable, how many high-order (most-significant) bits of the route_prefix will need to match the corresponding bits of the datagram's destination address?
@@ -29,14 +29,42 @@ void Router::add_route(const uint32_t route_prefix,
     cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/" << int(prefix_length)
          << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num << "\n";
 
-    DUMMY_CODE(route_prefix, prefix_length, next_hop, interface_num);
     // Your code here.
+    _route_table.push_back({route_prefix, prefix_length, next_hop, interface_num});
+    DUMMY_CODE(route_prefix, prefix_length, next_hop, interface_num);
 }
 
 //! \param[in] dgram The datagram to be routed
 void Router::route_one_datagram(InternetDatagram &dgram) {
-    DUMMY_CODE(dgram);
     // Your code here.
+    int forward_interface = -1;
+    uint32_t dst_ip = dgram.header().dst;
+    uint8_t longest = 0;
+    for (auto &route : _route_table) {
+        if (route.prefix_length >= longest) {
+            uint32_t mask =
+                route.prefix_length == 0 ? 0 : (0xffffffff << (32 - route.prefix_length));
+            if ((dst_ip & mask) == (route.route_prefix & mask)) {
+                longest = route.prefix_length;
+                forward_interface = route.interface_num;
+            }
+        }
+    }
+
+    // If the TTL was zero already, or hits zero after the decrement, the router should drop the datagram
+    if (forward_interface == -1 || dgram.header().ttl <= 1)
+        return;
+    dgram.header().ttl--;
+    // If the router is directly attached to the network in question, the next hop will be an empty optional.  In
+    // that case, the next hop is the datagram¡¯s destination address.
+    // But if the router is
+    // connected to the network in question through some other router, the next hop will
+    // contain the IP address of the next router along the path.
+    if (_route_table[forward_interface].next_hop.has_value())
+        interface(forward_interface).send_datagram(dgram, _route_table[forward_interface].next_hop.value());
+    else
+        interface(forward_interface).send_datagram(dgram, Address::from_ipv4_numeric(dgram.header().dst));
+    DUMMY_CODE(dgram);
 }
 
 void Router::route() {
--- libsponge/router.hh
+++ libsponge/router.hh
@@ -44,6 +44,15 @@ class Router {
     //! The router's collection of network interfaces
     std::vector<AsyncNetworkInterface> _interfaces{};
 
+    struct Table {
+        const uint32_t route_prefix;
+        const uint8_t prefix_length;
+        const std::optional<Address> next_hop;
+        const size_t interface_num;
+    };
+
+    std::vector<Table> _route_table{};
+
     //! Send a single datagram from the appropriate outbound interface to the next hop,
     //! as specified by the route with the longest prefix_length that matches the
     //! datagram's destination address.
